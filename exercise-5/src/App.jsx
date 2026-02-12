bafe0868d7128079239bf868a7d52a673b325518
Sereyrathanak <Sereyrathanak.rith@student.cadt.edu.kh>
2026-02-12 21:04:46 +0700

    finihed exercise 5

--- exercise-5/src/App.jsx
+++ exercise-5/src/App.jsx
@@ -1,5 +1,23 @@
+import "./index.css";
+import { Cardsdata } from "./data.js";
+
 function App() {
-  return <>{/* Your code  here */}</>;
+  return (
+    <div className="cards-container">
+      {Cardsdata.map((card) => (
+        <div key={card.id} className="card">
+          <div className="card-photo">
+            <img src={card.image} alt={card.name} />
+          </div>
+          <div className="card-info">
+            <h2>{card.name}</h2>
+            <p className="class">{card.className}</p>
+            <p className="hobbies">{card.hobbies}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
--- exercise-5/src/data.js
+++ exercise-5/src/data.js
@@ -1,1 +1,36 @@
-/* Your data here */
+
+import ding from "../html-sample-code/assets/1.png";
+import dong from "../html-sample-code/assets/2.png";
+import dang from "../html-sample-code/assets/3.png";
+import dung from "../html-sample-code/assets/4.png";
+
+export const Cardsdata = [
+  {
+    id: "p1",
+    name: "Ding",
+    className: "Class C",
+    hobbies: "Soccer, Tennis",
+    image: ding,
+  },
+  {
+    id: "p2",
+    name: "Dong",
+    className: "Class A",
+    hobbies: "Piano",
+    image: dong,
+  },
+  {
+    id: "p3",
+    name: "Dang",
+    className: "Class C",
+    hobbies: "Video Games",
+    image: dang,
+  },
+  {
+    id: "p4",
+    name: "Dung",
+    className: "Class B",
+    hobbies: "I like all",
+    image: dung,
+  },
+];
